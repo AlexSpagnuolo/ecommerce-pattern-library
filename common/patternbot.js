d75c46fcfbe4db35ebd3ec72c3fc15947061ca18
Alex Spagnuolo <spag0022@algonquinlive.com>
2018-02-28 22:06:20 -0500

    Add company input

--- common/patternbot.js
+++ common/patternbot.js
@@ -348,9 +348,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/Alex/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 2/Web Development IV/Week 02 - eCommerce UX/ecommerce/ecommerce-pattern-library
- * @version 1519866192314
+ * @version 1519872493612
  */
-const patternManifest_1519866192314 = {
+const patternManifest_1519872493611 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -672,6 +672,12 @@ const patternManifest_1519866192314 = {
       "path": "/Users/Alex/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 2/Web Development IV/Week 02 - eCommerce UX/ecommerce/ecommerce-pattern-library/patterns/forms",
       "html": [
         {
+          "name": "company-input",
+          "namePretty": "Company input",
+          "path": "/Users/Alex/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 2/Web Development IV/Week 02 - eCommerce UX/ecommerce/ecommerce-pattern-library/patterns/forms/company-input.html",
+          "localPath": "patterns/forms/company-input.html"
+        },
+        {
           "name": "discountcode-input",
           "namePretty": "Discountcode input",
           "path": "/Users/Alex/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 2/Web Development IV/Week 02 - eCommerce UX/ecommerce/ecommerce-pattern-library/patterns/forms/discountcode-input.html",
@@ -766,5 +772,5 @@ const patternManifest_1519866192314 = {
   }
 };
 
-patternBotIncludes(patternManifest_1519866192314);
+patternBotIncludes(patternManifest_1519872493611);
 }());
