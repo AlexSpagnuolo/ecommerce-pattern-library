13fa9fed80009de716522fb6df8cbba29e764ea7
Alex Spagnuolo <spag0022@algonquinlive.com>
2018-04-05 10:36:37 -0400

    Finish products page
    Add buttons

--- common/patternbot.js
+++ common/patternbot.js
@@ -388,9 +388,9 @@ const patternBotIncludes = function (manifest) {
 /** 
  * Patternbot library manifest
  * /Users/Test/Library/Mobile Documents/com~apple~CloudDocs/Year 2/Semester 5/Web Development IV/Week 02 - eCommerce UX/ecommerce/ecommerce-pattern-library
- * @version 1522937768258
+ * @version 1522938978537
  */
-const patternManifest_1522937768258 = {
+const patternManifest_1522938978537 = {
   "commonInfo": {
     "modulifier": [
       "responsive",
@@ -1080,5 +1080,5 @@ const patternManifest_1522937768258 = {
   }
 };
 
-patternBotIncludes(patternManifest_1522937768258);
+patternBotIncludes(patternManifest_1522938978537);
 }());
